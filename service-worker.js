823d2550a4d73cd7641cad7c042e043b25f64756
grafiume <grafiume@hotmail.it>
2025-10-14 15:40:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-const CACHE='officina-cache-v5';
-const ASSETS=[
-  './','./index.html','./manifest.json',
-  './js/db.js','./js/app.js','./js/cloud-sync.js','./config.js',
-  './icons/icon-192.png','./icons/icon-512.png','./logo-elip.jpg'
-];
+const CACHE='officina-cache-v6';
+const ASSETS=['./','./index.html','./manifest.json','./js/db.js','./js/app.js','./js/cloud-sync.js','./config.js','./icons/icon-192.png','./icons/icon-512.png','./logo-elip.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(h=>h||fetch(e.request).catch(()=>caches.match('./index.html')))
-  );
-});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request).catch(()=>caches.match('./index.html'))));});
