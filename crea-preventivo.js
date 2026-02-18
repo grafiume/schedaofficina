e8c858c6ab63dc592764d896a0492537a02d478e
grafiume <grafiume@hotmail.it>
2026-02-18 16:16:43 +0100

    Update crea-preventivo.js

--- crea-preventivo.js
+++ crea-preventivo.js
@@ -22,7 +22,7 @@
   const PREVENTIVI_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dndjc29xc29xanlxd2twZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2OTk1MDAsImV4cCI6MjA3NjI3NTUwMH0.phXUlrbGx-f4wIjYAaG8JhTFHcp8utBCWwUvkTQMnKs";
 
   async function callCreatePreventivo(payload) {
-    const endpoint = `${PREVENTIVI_URL}/functions/v1/create-preventivo-from-scheda`;
+    const endpoint = `${PREVENTIVI_URL}/functions/v1/rapid-worker`;
 
     const resp = await fetch(endpoint, {
       method: "POST",
